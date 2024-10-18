9e1f0c1df5af0d1651ac3137e51469e4dd1aa976
Conrad Salazar <themetroid7998@gmail.com>
2024-10-17 22:23:40 -0700

    CH8 Exercises complete (within reason).

--- CIS12_Exercises_CH8.py
+++ CIS12_Exercises_CH8.py
@@ -1,4 +1,4 @@
-
+import re
 
 """8.12.2 -- Brought to you by ChatGPT"""
 def head_dirty(file_path, lines, output_path):
@@ -57,11 +57,45 @@ def uses_any(word, chars):
 def wordle(word, chars, answer):
     word = word.strip().lower()
     chars = chars.strip.lower()
+    pass
 
-    if word > 5:
+def check_word(word):
+    if len(word) > 5:
         return print("5 letter words only!")
+    else: pass
 
-    if uses_any(word, chars):
-        return print(f"This word is partially correct; {uses_any} is/are in the word but may or may not be in the right spot.")
-    return print("This word is completely incorrect.")
+    pattern = r'\b(?: \w{2} E \w | \w{4} E \w | \w_[chars]\w_)\b'
+    # okay, fairly new to this... had to copy you. as I understand it,
+    # you have the word boundary \b
+    # I dont really get the ?: thing, moving on...
+    # \w{2} takes any two characters, then a capital E then any one character OR
+    # \w{4} takes any four characters, then E, then any one character OR
+    # \w takes any one character, [] takes any of the set of characters, then any one character
+    if not re.match(pattern, word, re.IGNORECASE):
+        return False, print("This word is incorrect.")
+    else: return True
 
+def give_clue():
+    count = 0
+    with open('words.txt', 'r') as file:
+        for word in file:
+            word = word.strip()
+            if len(word) == 5 and check_word(word):
+                print(word)
+                count += 1
+
+    print(f"A total of {count} words could still match.")
+
+"""8.12.5"""
+
+def word_check():
+    with open('TheCountofMnteCristo.txt', 'r') as book:
+        count = 0
+        for line in book:
+            pattern = r'\b(?:pale(s | d | ness)? | pallor)\b'
+            sentence = line.strip()
+            if re.match(pattern, sentence):
+                print(line)
+                count += 1
+
+    print(f"Lines with some form of pale: {count}")
