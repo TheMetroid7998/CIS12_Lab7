27920a34ede29cda25938f1529998fcc9210b0f8
Conrad Salazar <themetroid7998@gmail.com>
2024-10-17 21:39:22 -0700

    CH9 Exercises complete.

--- CIS12_Exercises_CH9.py
+++ CIS12_Exercises_CH9.py
@@ -20,14 +20,28 @@ def anagram_finder():
 def is_palindrome(str0):
     return str0 == str0[::-1]
 
-def palindrome_finder(): # TODO: Fix.
+def palindrome_finder():
     count = 0
     with open('words.txt', 'r', encoding = "utf_8") as textfile:
-        for word in textfile:
-            word = word.strip().lower()
+        for i, line in enumerate(textfile):
+            word = line.strip().lower()
             if len(word) >= 7 and is_palindrome(word):
-                print(word)
+                print(f"Found {word} on line {i}")
                 count += 1
-    print(f"Found {count} palindromes in words.txt")
+    print(f"Found {count} total palindromes in words.txt")
 
-palindrome_finder()
+"""9.15.4"""
+def reverse_sentence(input_string):
+    str_list = input_string.split(' ')
+    str_list[0] = str(str_list[0]).lower()
+    rev_list = str_list[::-1]
+    rev_list[0] = str(rev_list[0]).capitalize()
+    output_string = ' '.join(rev_list)
+    print(output_string)
+
+"""9.15.5"""
+def total_length():
+    count = 0
+    with (open('words.txt', 'r', encoding="utf_8") as textfile):
+        words_list = textfile.readlines()
+        return sum(len(w.strip()) for w in words_list)
